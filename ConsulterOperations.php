9506b29570a765374d16309fca69843f3d437dc6
Laurent Tricard <ltricard@carboatmedia.fr>
2018-01-11 21:32:27 +0100

    tri sur les opérations

--- ConsulterOperations.php
+++ ConsulterOperations.php
@@ -11,9 +11,11 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" type="text/css" href="./style/style.css">
-        <script type="text/javascript" language="javascript" src="media/js/jquery.js"></script>
-        <script type="text/javascript" language="javascript" src="media/js/jquery.dataTables.js"></script>
+	    <link rel="stylesheet" type="text/css" href="./style/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/datatables.min.css">
+
+        <script type="text/javascript" language="javascript" src="assets/js/vendor/jquery.min.js"></script>
+        <script type="text/javascript" language="javascript" src="assets/js/vendor/datatables.min.js"></script>
 
         
         <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
@@ -25,7 +27,7 @@
         <script type="text/javascript" language="javascript" src="//cdn.datatables.net/plug-ins/1.10.16/sorting/num-html.js"></script>
         <script type="text/javascript" src="fonctions.js"></script>
 
-        <script src='./js/jquery.min.js'></script>
+<!--        <script src='./js/jquery.min.js'></script>-->
 <!--        <script>window.jQuery || document.write('<script src= \"./assets/js/vendor/jquery.min.js \"><\/script>')</script>-->
         <script src='./dist/js/bootstrap.min.js'></script>
         <script src='./assets/js/docs.min.js'></script>
@@ -33,7 +35,7 @@
 
     </head>
     <body class="body">
-        <!--Center pour que le tableau soit centr�-->
+        <!--Center pour que le tableau soit centr�-->
         <center>
             <table bgcolor="white" width="1200" border="0" cellspacing="0" cellpadding="30" marginwidth="1200" marginheight="">
                 <tr>
@@ -93,30 +95,18 @@
 			$(document).ready(function() 
 				{
 
-					$.fn.dataTable.moment( 'DD/MM/YYYY - HH:mm:ss' )
-					$('#operations')
+				//	$.fn.dataTable.moment( 'DD/MM/YYYY - HH:mm:ss' )
 
-						/*{
-
-							"language" : 	{ 
-										"decimal" : '.',
-										"thousands" : ' ',
-										"url": "dataTables.french.lang"
-									}
-						},*/
-						
-						{
-                                                            aaSorting: [[ 0, "desc" ]]
-						},
-
-						
-
-						{
-                                                    columnDefs:	[
-										{type: "any-number", targets : 2 }
-									]
-
-					  	}
+                    $('#operations').dataTable({
+                        "paging":   false, // pas de pagination
+                        "ordering": true,
+                        "info":     false,
+                        "language": { // aide à trier correctement le prix
+                            "decimal": ".",
+                            "thousands": ","
+                        },
+                        "order": [[ 0, "desc" ]] // défini le tri par défaut (peut-être doublon avec la requête sql quand on arrive sur la page)
+                    });
 				} 
 			);
 
